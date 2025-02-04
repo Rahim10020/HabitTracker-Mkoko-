f8810047cdf67d00ad9f4b9035062cc85eab13a8
Rahim10020 <rahialighi@gmail.com>
2025-02-04 23:52:02 +0000

    Finition de la base de donnees et debut du UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:pro3_flutter/database/habit_database.dart';
 import 'package:pro3_flutter/pages/home_page.dart';
 import 'package:pro3_flutter/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
-      child: const MyApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  // we gonna initialize the database
+  await HabitDatabase.initialize();
+  // and we gonna save the first launch date
+  await HabitDatabase().saveFirstLaunchDate();
+  runApp(MultiProvider(
+    providers: [
+      // habit provider
+      ChangeNotifierProvider(create: (context) => HabitDatabase()),
+      // theme provider
+      ChangeNotifierProvider(create: (context) => ThemeProvider())
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
