7d36c56a953405a37040d7465dfea1c7e025affc
Rahim10020 <rahialighi@gmail.com>
2026-09-04 12:51:58 +0000

    add pill

---
+++ lib/components/pill_nav_bar.dart
@@ -0,0 +1,95 @@
+import 'package:R_HabitTracker/theme/app_spacing.dart';
+import 'package:flutter/material.dart';
+
+class PillNavItem {
+  final IconData icon;
+
+  const PillNavItem({required this.icon});
+}
+
+class PillNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onChanged;
+  final List<PillNavItem> items;
+  final double height;
+
+  const PillNavBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onChanged,
+    required this.items,
+    this.height = 56,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final radius = BorderRadius.circular(AppRadius.full);
+
+    return Container(
+      height: height,
+      decoration: BoxDecoration(
+        color: colorScheme.surface,
+        borderRadius: radius,
+        boxShadow: [
+          BoxShadow(
+            color: colorScheme.shadow.withValues(alpha: 0.08),
+            blurRadius: 12,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Material(
+        color: Colors.transparent,
+        borderRadius: radius,
+        clipBehavior: Clip.antiAlias,
+        child: Row(
+          children: [
+            for (var index = 0; index < items.length; index++)
+              Expanded(
+                child: _PillNavItem(
+                  icon: items[index].icon,
+                  selected: index == selectedIndex,
+                  height: height,
+                  onTap: () => onChanged(index),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _PillNavItem extends StatelessWidget {
+  final IconData icon;
+  final bool selected;
+  final double height;
+  final VoidCallback onTap;
+
+  const _PillNavItem({
+    required this.icon,
+    required this.selected,
+    required this.height,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(AppRadius.full),
+      child: SizedBox(
+        height: height,
+        child: Center(
+          child: Icon(
+            icon,
+            color:
+                selected ? colorScheme.primary : colorScheme.onSurfaceVariant,
+          ),
+        ),
+      ),
+    );
+  }
+}
